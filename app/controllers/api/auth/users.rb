e383398dfa027a37bc4bb978375d577c5bd13b33
NickBonet <nbonet@kshadow.us>
2020-04-19 17:59:53 -0400

    Much needed regex matching added.

--- app/controllers/api/auth/users.rb
+++ app/controllers/api/auth/users.rb
@@ -3,14 +3,15 @@ module API
     class Users < Grape::API
       version 'v1', using: :header, vendor: 'K-Shadow'
       format :json
+      password_regex = %r{^(?=.*\d)(?=.*[~!@#$%^&*()_\-+=|\\\{\}\[\]:;<>?\/])(?=.*[A-Z])(?=.*[a-z])\S{8,40}$}
 
       helpers do
         # If the client is even sending an Authorization header, ignore the request.
         def authorized?
-          if headers.key?("Authorization")
-            return true
+          if headers.key?('Authorization')
+            true
           else
-            return false
+            false
           end
         end
       end
@@ -18,23 +19,24 @@ module API
       resource :users do
         desc 'Add a new user to the database.'
         params do
-          requires :username, type: String
-          requires :email, type: String
-          requires :password, type: String
-          requires :password_confirm, type: String
+          requires :username, type: String, regexp: /^[A-Za-z][A-Za-z0-9_-]{3,29}$/
+          requires :email, type: String,
+                           regexp: URI::MailTo::EMAIL_REGEXP
+          requires :password, type: String, regexp: password_regex
+          requires :password_confirm, type: String, regexp: password_regex
         end
         post do
-          if not authorized?
-          if params[:password] == params[:password_confirm]
-            User.create!(name: params[:username], email: params[:email], password: params[:password])
-          else
-            body 'Passwords do not match!'
-            status 422
-          end
+          if !authorized?
+            if params[:password] == params[:password_confirm]
+              User.create!(name: params[:username], email: params[:email], password: params[:password])
+            else
+              body 'Passwords do not match!'
+              status 422
+            end
           else
             body 'You are already logged in!'
             status 403
-          end 
+          end
         end
       end
 
@@ -42,8 +44,8 @@ module API
         use Grape::Knock::Authenticable
         desc "Update a user's details."
         params do
-          requires :password, type: String
-          requires :password_confirm, type: String
+          requires :password, type: String, regexp: password_regex
+          requires :password_confirm, type: String, regexp: password_regex
         end
         put do
           if params[:password] == params[:password_confirm]
