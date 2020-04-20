704c39eb66aaa41d2ee15a401d7e5003111dcb77
NickBonet <nbonet@kshadow.us>
2020-04-20 13:20:42 -0400

    Add rack-attack to protect the API some!

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,32 @@
+class Rack::Attack
+  Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+
+  throttle('req/ip', limit: 100, period: 5.minutes) do |req|
+    req.ip
+  end
+
+  throttle('logins/ip', limit: 3, period: 20.seconds) do |req|
+    if req.path == '/auth/user_token'
+      req.ip
+    end
+  end
+
+  throttle('signup/ip', limit: 5, period: 1.day) do |req|
+    if req.path == '/auth/users' && req.post?
+      req.ip
+    end
+  end
+
+
+  throttle('update user/ip', limit: 5, period: 1.day) do |req|
+    if req.path == '/auth/users' && req.put?
+      req.ip
+    end
+  end
+
+  throttle("logins/email", limit: 3, period: 20.seconds) do |req|
+    if req.path == '/auth/user_token' && req.post?
+      req.params['auth']['email'].presence
+    end
+  end
+end
